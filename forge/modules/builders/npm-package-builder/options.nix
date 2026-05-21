7d8d755991e3e41930ee2ebd7f67b4ce8d802217
Ivan Mincik <ivan.mincik@gmail.com>
2026-05-21 10:15:51 +0200

    docs: update packages.*.build.npmPackageBuilder docs

--- forge/modules/builders/npm-package-builder/options.nix
+++ forge/modules/builders/npm-package-builder/options.nix
@@ -5,28 +5,36 @@
 {
   options.build.npmPackageBuilder = {
     enable = lib.mkEnableOption ''
-      NPM package builder for JavaScript/TypeScript packages.
+      NPM package builder for JavaScript and TypeScript packages.
 
-      Uses buildNpmPackage'';
+      Uses `buildNpmPackage` from Nixpkgs, which builds Node.js packages
+      using `npm ci` with a locked dependency set from `package-lock.json`.
+      Node.js is automatically included as a build-time dependency.
+
+      For more information, see the
+      [Nixpkgs Node.js documentation](https://nixos.org/manual/nixpkgs/unstable/#language-javascript)
+    '';
 
     packages = {
       build = lib.mkOption {
         type = lib.types.listOf lib.types.package;
         default = [ ];
         description = ''
-          Build-time dependencies (native architecture).
+          List of additional build-time dependencies needed during compilation (native architecture).
 
-          Tools needed during compilation that run on the build machine.
+          Node.js is included automatically.
+
+          Mapped to `nativeBuildInputs`.
         '';
-        example = lib.literalExpression "[ pkgs.nodejs ]";
+        example = lib.literalExpression "[ pkgs.pkg-config ]";
       };
       run = lib.mkOption {
         type = lib.types.listOf lib.types.package;
         default = [ ];
         description = ''
-          Runtime dependencies (target architecture).
+          List of runtime dependencies needed by the package (target architecture).
 
-          Libraries needed by the package at runtime.
+          Mapped to `buildInputs`.
         '';
         example = lib.literalExpression "[ pkgs.vips ]";
       };
@@ -34,10 +42,11 @@
         type = lib.types.listOf lib.types.package;
         default = [ ];
         description = ''
-          Test dependencies.
+          List of test dependencies needed to run the test suite.
 
-          Packages needed to run tests.
+          Mapped to `nativeCheckInputs`.
         '';
+        example = lib.literalExpression "[ pkgs.chromium ]";
       };
     };
 
@@ -45,9 +54,11 @@
       type = lib.types.str;
       default = "";
       description = ''
-        SHA256 hash of the package-lock.json file or source.
+        Hash of the npm dependencies fetched from `package-lock.json`.
 
-        Leave empty initially - nix will provide the correct hash on first build.
+        Leave empty initially to let Nix print the correct hash on first build.
+
+        Mapped to `npmDepsHash`.
       '';
       example = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
     };
@@ -56,11 +67,11 @@
       type = lib.types.listOf lib.types.str;
       default = [ ];
       description = ''
-        Flags to pass to `npm ci`.
+        Additional flags passed to `npm ci` during the install phase.
+
+        Mapped to `npmInstallFlags`.
       '';
-      example = [
-        "--ignore-scripts"
-      ];
+      example = lib.literalExpression ''[ "--ignore-scripts" ]'';
     };
   };
 }
