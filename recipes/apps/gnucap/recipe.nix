daaea9a0e98ff17516e633d439090c24950bf554
Ivan Mincik <ivan.mincik@gmail.com>
2026-06-11 20:40:18 +0200

    recipes: add gnucap app recipe

---
+++ recipes/apps/gnucap/recipe.nix
@@ -0,0 +1,66 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  apps.gnucap = {
+    displayName = "Gnucap";
+    description = "GNU Circuit Analysis Package.";
+    usage = ''
+      Gnucap is a general purpose circuit simulator that performs DC, transient,
+      and AC analyses, and supports Spice-compatible netlists.
+
+      Start the interactive Gnucap shell
+
+      ```bash
+      gnucap
+      ```
+
+      Run a simulation from a netlist file
+
+      ```bash
+      gnucap -b circuit.ckt
+      ```
+    '';
+
+    icon = ./icon.svg;
+
+    links = {
+      website = "http://www.gnucap.org/";
+      docs = "https://www.gnu.org/software/gnucap/gnucap-man.pdf";
+    };
+
+    ngi.grants = {
+      Commons = [
+        "Gnucap-performance"
+      ];
+      Entrust = [
+        "Gnucap-MixedSignals"
+        "Gnucap-VerilogAMS"
+      ];
+    };
+
+    programs = {
+      packages = [
+        pkgs.gnucap
+      ];
+
+      runtimes.shell = {
+        enable = true;
+      };
+    };
+
+    test.programs.script = ''
+      cat <<EOF > circuit.ckt
+      * Simple resistor circuit
+      V1 1 0 DC 5
+      R1 1 0 1k
+      .DC V1 5 5 1
+      .print DC V(1)
+      .end
+      EOF
+      gnucap -b circuit.ckt | grep -q "5"
+    '';
+  };
+}
