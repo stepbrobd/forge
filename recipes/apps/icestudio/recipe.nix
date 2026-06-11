03e51b7818ab99fb381b73fb2d0e26b2d50fdc9f
Ivan Mincik <ivan.mincik@gmail.com>
2026-06-11 20:40:09 +0200

    recipes: add icestudio app recipe

---
+++ recipes/apps/icestudio/recipe.nix
@@ -0,0 +1,33 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  apps.icestudio = {
+    displayName = "Icestudio";
+    description = "Visual developer tool for development of FPGAs.";
+    usage = ''
+      Icestudio is a visual editor for open FPGA boards that lets you design
+      digital circuits using a block-based graphical interface.
+    '';
+
+    icon = ./icon.svg;
+
+    links = {
+      website = "https://icestudio.io/";
+      source = "https://github.com/FPGAwars/icestudio";
+    };
+
+    ngi.grants = {
+      Entrust = [
+        "Icestudio"
+      ];
+    };
+
+    programs = {
+      mainPackage = pkgs.icestudio;
+      runtimes.program.enable = true;
+    };
+  };
+}
