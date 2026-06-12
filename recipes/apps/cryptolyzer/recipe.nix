abc8359e05d321bf481f64f5c492c99f16b5a378
Ivan Mincik <ivan.mincik@gmail.com>
2026-06-12 11:31:19 +0200

    recipes: add cryptolyzer app recipe

---
+++ recipes/apps/cryptolyzer/recipe.nix
@@ -0,0 +1,57 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  apps.cryptolyzer = {
+    displayName = "CryptoLyzer";
+    description = "Cybersecurity tool that can analyse cryptographic settings of clients and servers for different protocols.";
+    usage = ''
+      CryptoLyzer analyses the cryptographic settings of TLS, SSH, and other
+      protocol servers and clients, and tests endpoints against known vulnerabilities.
+
+      Analyse TLS settings of a server
+
+      ```bash
+      cryptolyze tls all example.com
+      ```
+
+      Analyse SSH settings of a server
+
+      ```bash
+      cryptolyze ssh2 pubkeys example.com
+      ```
+    '';
+
+    icon = ./icon.svg;
+
+    links = {
+      website = "https://gitlab.com/coroner/cryptolyzer";
+      docs = "https://cryptolyzer.readthedocs.io/en/latest/";
+    };
+
+    ngi.grants = {
+      Core = [
+        "CryptoLyzer-IKE"
+      ];
+      Review = [
+        "CryptoLyzer"
+      ];
+    };
+
+    programs = {
+      packages = [
+        pkgs.cryptolyzer
+      ];
+
+      runtimes.shell = {
+        enable = true;
+      };
+    };
+
+    test.programs.script = ''
+      cryptolyze --help 2>&1 | grep -qi "usage"
+    '';
+  };
+}
